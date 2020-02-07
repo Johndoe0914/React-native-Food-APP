302956ab76ba8aba005e185c36d098b2a56f56c8
jonathan diaz <jonathandiaz0914@gmail.com>
2020-02-07 15:18:22 -0500

    added hooks with filtering

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,42 +1,41 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, Text, StyleSheet} from "react-native";
 import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
+import useResults from '../hooks/useResults';
+import ResultsList from "../components/ResultsList";
 
 
 
 const SearchScreen = () => {
     
     const [term, setTerm] = useState('');
-    const [results, setResults] = useState('')
-    const [ errorMessage, setErrorMessage] = useState('')
+    const [ searchApi, results, errorMessage] = useResults();
 
-    const searchApi = async () => {
-      try {
-        const response = await yelp.get('/search', {
-            params: {
-                limit: 50,
-                term,
-                location:'kissimmee'
- 
-            }
-        })
-        setResults(response.data.businesses)
-      } catch(err) {
-        setErrorMessage('Something Went Wrong')
-
-    
-    }
+    const filterResultsByPrice = price => {
+        // price === '$' || '$$' || '$$$'
+        if(results.length){
+            return results.filter(result => {
+                return result.price === price;
+              });
+            
+        } else{
+            return results.length
+        }
+      
+      };
     
     return (
         <View>
-            <SearchBar onTermSubmit={searchApi} term={term} onTermChange={setTerm} />
+    <SearchBar onTermSubmit={() => searchApi(term)} term={term} onTermChange={setTerm} />
     {errorMessage ? <Text>{errorMessage}</Text> : null}
     <Text>We have found {results.length}</Text>
+        <ResultsList title="Cost Effective" results={filterResultsByPrice("$")} />
+        <ResultsList title="Bit Pricier" results={filterResultsByPrice("$$")}/>
+        <ResultsList title="Big Spender"results={filterResultsByPrice("$$$")}/> 
         </View>
     )
-}}
-
+}
 const styles = StyleSheet.create({
 })
 
